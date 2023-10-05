156dacc2e36a26074ffa5c11ae6ed0e7b65ff5a2
thamara <thamaragerigr@gmail.com>
2023-10-05 08:46:11 +0200

    feat: add prject

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # url-shortener
+
+Go url shortener following this tutorial: https://courses.calhoun.io/lessons/les_goph_05 by John Calhoun.
+
+## Learning
+
+* Parsing YAML files 
+* Use of `"net/http"` to redirect URLs
+
+## To do's
+
+* Add tests
---
+++ urlshort/handler.go
@@ -0,0 +1,49 @@
+package urlshort
+
+import (
+	"net/http"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if dest, ok := pathsToUrls[path]; ok {
+			http.Redirect(w, r, dest, http.StatusFound)
+			return
+		}
+		fallback.ServeHTTP(w, r)
+	}
+}
+
+func YAMLHandler(yamlBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	pathUrls, err := parseYaml(yamlBytes)
+	if err != nil {
+		return nil, err
+	}
+	pathsToUrls := buildMap(pathUrls)
+	return MapHandler(pathsToUrls, fallback), nil
+}
+
+func buildMap(pathUrls []pathUrl) map[string]string {
+	pathsToUrls := make(map[string]string)
+	for _, pu := range pathUrls {
+		pathsToUrls[pu.Path] = pu.URL
+	}
+	return pathsToUrls
+}
+
+func parseYaml(data []byte) ([]pathUrl, error) {
+	var pathUrls []pathUrl
+	err := yaml.Unmarshal(data, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+	return pathUrls, nil
+}
+
+type pathUrl struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
+}
